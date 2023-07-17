81d96c3bb6b19f1c5394f3e6944eb6214c241b6d
SyedaFarhin <syedafarhin18@gmail.com>
2023-07-18 00:01:42 +0530

    changes

---
+++ codepen/src/components/Code.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Box, styled } from "@mui/material";
+import Editor from "./Editor";
+import { DataContext } from "../context/DataProvider";
+import { useContext } from "react";
+const Container = styled(Box)`
+  background-color: #060606;
+  height: 50vh;
+  display: flex;
+`;
+const Code = () => {
+  const { html, css, js, setHtml, setCss, setJs } = useContext(DataContext);
+  return (
+    <Container>
+      <Editor
+        language="xml"
+        heading="HTML"
+        value={html}
+        onChange={setHtml}
+        icon="/"
+        color="#FF3C41"
+      />
+      <Editor
+        language="css"
+        heading="CSS"
+        value={css}
+        onChange={setCss}
+        icon="*"
+        color="#0EBEFF"
+      />
+      <Editor
+        language="javascript"
+        heading="JS"
+        value={js}
+        onChange={setJs}
+        icon="( )"
+        color="#FCD000"
+      />
+    </Container>
+  );
+};
+
+export default Code;
